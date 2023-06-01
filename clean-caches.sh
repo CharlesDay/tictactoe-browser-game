e65ce8b2d53664aabe5488960c06c6f2c31819a5
Charles Day <cdayjr1997@gmail.com>
2023-05-31 23:47:10 -0400

    good

--- clean-caches.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-  curl -L \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GITHUB_TOKEN"\
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/CharlesDay/tictactoe-browser-game/actions/caches
